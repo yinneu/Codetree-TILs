7d619574b861ce723c3c144212dee5c58a2f05c9
YerimYeon <99879845+yinneu@users.noreply.github.com>
2025-10-01 14:31:03 +0900

    Title: Time to Time, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251001/Time to Time/time-to-time.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        // Please write your code here.
+
+        int first = 2 * 60 + 5;
+        int second = 4 * 60 + 1;
+
+        System.out.println(second - first);
+    }
+}
